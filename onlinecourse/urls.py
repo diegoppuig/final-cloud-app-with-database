8a80aa9ffb1dfb57a8d782cb80b232eb9a33b59e
diegoppuig <diego.puig27@gmail.com>
2023-03-25 18:52:15 -0400

    lab 3 done

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -1,14 +1,32 @@
-from django.urls import path
-from django.conf import settings
+"""myproject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
 from django.conf.urls.static import static
-from . import views
+from django.conf import settings
+
+from my_app import views
 
 app_name = 'onlinecourse'
+
 urlpatterns = [
-    # route is a string contains a URL pattern
-    # view refers to the view function
-    # name the URL
-    path(route='', view=views.CourseListView.as_view(), name='index'),
+    path('admin/', admin.site.urls),
+    path('onlinecourse/', include('onlinecourse.urls')),
+
+     path(route='', view=views.CourseListView.as_view(), name='index'),
     path('registration/', views.registration_request, name='registration'),
     path('login/', views.login_request, name='login'),
     path('logout/', views.logout_request, name='logout'),
@@ -18,7 +36,7 @@ urlpatterns = [
     path('<int:course_id>/enroll/', views.enroll, name='enroll'),
 
     # <HINT> Create a route for submit view
-
+    path('<int:course_id>/submit/', views.submit, name='submit'),
     # <HINT> Create a route for show_exam_result view
-
- ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('course/<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result, name='show_exam_result'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -135,7 +135,14 @@ def submit(request, course_id):
 #            choice_id = int(value)
 #            submitted_anwsers.append(choice_id)
 #    return submitted_anwsers
-
+def extract_answers(request):
+    submitted_anwsers = []
+    for key in request.POST:
+        if key.startswith('choice'):
+            value = request.POST[key]
+            choice_id = int(value)
+            submitted_anwsers.append(choice_id)
+    return submitted_anwsers
 
 # <HINT> Create an exam result view to check if learner passed exam and show their question results and result for each question,
 # you may implement it based on the following logic:
@@ -144,6 +151,39 @@ def submit(request, course_id):
         # For each selected choice, check if it is a correct answer or not
         # Calculate the total score
 #def show_exam_result(request, course_id, submission_id):
+def show_exam_result(request, course_id, submission_id):
+    course = get_object_or_404(Course, pk=course_id)
+    submission = get_object_or_404(Submission, pk=submission_id)
+    total = 0
+    total_user = 0
+    q_results = {}
+    c_submits = {}
+    c_results = {}
+    for q in course.question_set.all():
+        q_total = 0
+        q_total_user = 0
+        for c in q.choice_set.all():
+            q_total += 1
+            temp_right = c.is_correct
+            count = submission.choices.filter(id = c.id).count()
 
+            temp_user = count > 0
+            c_submits[c.id] = temp_user
+            c_results[c.id] = temp_user == temp_right
+            if temp_user == temp_right:
+                q_total_user += 1
+        q_results[q.id] = q.grade*(q_total_user / q_total)
+        total += q.grade
+        total_user += q_results[q.id]
 
+    context = {}
+    context["course"] = course 
+    context["submission"] = submission
+    context["total"] = total
+    context["total_user"] = total_user
+    context["q_results"]  =  q_results
+    context["c_submits"]  =  c_submits
+    context["c_results"]  =  c_results
+    context["grade"]  =  int((total_user/total)*100)
 
+    return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
